671ef253436fbf6fe994e547f038b84001e9c247
Ali Naseri <mo.ali.naseri@gmail.com>
2024-09-23 03:24:18 +0330

    chore: modify vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }: never) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_APP_PORT ?? '3000') || 3000,
+    },
+  });
+};
