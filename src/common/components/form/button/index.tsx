0b89cd298852f47fe35e71dc0ded96d1f1baff8d
Ali Naseri <mo.ali.naseri@gmail.com>
2024-09-23 23:12:27 +0330

    fix: remove extension in import

--- src/common/components/form/button/index.tsx
+++ src/common/components/form/button/index.tsx
@@ -1,12 +1,13 @@
 import styled from '@emotion/styled';
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
-import { buttonStyles } from './button.styles.ts';
+import { buttonStyles } from './button.styles';
 
 interface ButtonProps
   extends DetailedHTMLProps<
     ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
-  > {}
+  > {
+}
 
 export const Button = styled.button<ButtonProps>(buttonStyles);
